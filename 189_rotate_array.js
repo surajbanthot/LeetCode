08c3a20502667d1427d38bd15a84350c4e6d9f17
suraj <surajbanthot@gmail.com>
2021-02-05 20:52:51 +0530

    rotate array

---
+++ 189_rotate_array.js
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/rotate-array/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    for (let i = 0; i < k; i++) {
+       nums.unshift(nums.pop()) 
+   }
+   return nums;
+};
